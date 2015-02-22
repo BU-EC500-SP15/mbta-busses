006935509037b8f5444ca57d745d23793ed99db4
Huy Le <huyle333@bu.edu>
2015-02-22 17:47:13 -0500

    static function to process CSV complete data

--- html/js/bus-map.js
+++ html/js/bus-map.js
@@ -9,6 +9,40 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
+
+// manually grab the CSV file and process data
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "data.txt",
+        dataType: "text",
+        success: function(data){
+            processData(data);
+        }
+    });
+});
+
+function processData(text){
+    var record_num = 46;
+    var textLines = text.split(/\r\n|\n/);
+    var entries = textLines[0].split(',');
+    var lines = [];
+
+    var headings = entries.splice(0, record_num);
+    while (entries.length > 0){
+        var tarr = [];
+        for(var j = 0; j < record_num; j++){
+            tarr.push(headings[j] + ":" + entries.shift());
+        }
+        lines.push(tarr);
+    }
+}
+
+function routes(){
+    // infowindow = new google.maps.InfoWindow;
+
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 d3.csv("data/shapes.txt", function(d){
