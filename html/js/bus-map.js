594fe386ab565814b823d06c7be5efd0fb85d741
Huy Le <huyle333@bu.edu>
2015-02-22 16:30:34 -0500

    added preliminary Google maps using API to visualize 15 key bus routes

--- html/js/bus-map.js
+++ html/js/bus-map.js
@@ -1,5 +1,75 @@
+var map;
+
+function initialize() {
+    var mapOptions = {
+        center: {lat: 42.348570, lng: -71.095233},
+        zoom: 13,
+        mapTypeControl: false
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
 d3.csv("data/shapes.txt", function(d){
-	console.log(d);
+
+	var margin = {top: 10, right: 10, bottom: 20, left: 40},
+		width = 500 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+	var x = d3.scale.linear()
+    	.domain([-100, 100])
+    	.range([-100, width]);
+
+	var y = d3.scale.linear()
+    	.domain([0, 100])
+    	.range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+    	.scale(x)
+    	.orient("bottom");
+
+	var yAxis = d3.svg.axis()
+    	.scale(y)
+    	.orient("left");
+
+    var line = d3.svg.line()
+    	.x(function(d) {return x(d.shape_pt_lon); })
+    	.y(function(d) {return y(d.shape_pt_lat); });
+    	//.x(function(d) {return x(d.x); })
+    	//.y(function(d) {return y(d.y); });
+
+    var svg = d3.select("body").append("svg")
+    	.datum(d)
+    	.attr("width", width + margin.left + margin.right)
+    	.attr("height", height + margin.top + margin.bottom)
+      .append("g")
+    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("g")
+    	.attr("class", "x axis")
+    	.attr("transform", "translate(0," + height + ")")
+    	.call(xAxis);
+
+	svg.append("g")
+    	.attr("class", "y axis")
+    	.call(yAxis);
+
+	svg.append("path")
+    	.attr("class", "line")
+    	.attr("d", line);
+
+    /*
+    svg.selectAll(".dot")
+    	.data(d)
+      .enter().append("circle")
+      	.attr("class", "dot")
+      	.attr("cx", line.x())
+      	.attr("cy", line.y())
+      	.attr("r", 3.5);
+	*/
+
+    /*
 	return {
 		shape_id: d.shape_id,
 		shape_lat: d.shape_pt_lat,
@@ -7,6 +77,5 @@ d3.csv("data/shapes.txt", function(d){
 		shape_sequence: d.shape_pt_sequence,
 		shape_traveled: d.shape_dist_traveled
 	};
-}, function(error, rows){
-	console.log(rows);
+	*/
 });
