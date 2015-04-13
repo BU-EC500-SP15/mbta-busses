d87caad1783b4b9a46c09d2c270b0d29d8dd63af
GideonJura <zhangyue@bu.edu>
2015-04-13 19:21:18 -0400

    update time analysis

--- ShellScripts/TimeAnalysis.sh
+++ ShellScripts/TimeAnalysis.sh
@@ -3,25 +3,24 @@ RECOMMENDER_WORK_PATH=/disk6/datacenter/7_recommend/pushvideo/script
 DATA_PATH=/mbta-busses/DataSet
 RESULT_PATH=/hadoop/fs/...
 
-if [ $# != 1 ] ; then 
-	echo "USAGE: $0 file on the cluster" 
-	echo " e.g.(10am-12am): sh $0 oneday.csv" 
+if [ $# != 2 ] ; then 
+	echo "USAGE: $0 file on the cluster nameofshell" 
+	echo " e.g.(10am-12am): sh $0 oneday.csv HeadwayByTrip.sh" 
 	exit 1; 
 fi 
 
 ALL_DIRS=
 beg=0
 end=50
-begintime=$3
-endtime=$4
 
-for((;$beg<=2350;))
+for((;$beg<=1440;))
 do  
-	echo 'Doing Scirpt for these days'
-	sh DiffTime.sh $1 $beg $end 
-	sh TripDuration.sh $1 $beg $end
-	$beg=$beg+30
-	$end=$end+30
+	echo 'Doing Scirpt for $1'
+	sh $2 $1 $beg $end 
+	let beg=beg+30
+	let end=end+30
+	echo $beg
+	echo $end
 done
 
 
