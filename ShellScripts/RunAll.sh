1486649a7ca3602cf2ca9d5d1f47cddf7cf2dac3
GideonJura <zhangyue@bu.edu>
2015-05-02 14:42:28 -0400

    Schduled Running shell Script
    Stupid but works

--- ShellScripts/RunAll.sh
+++ ShellScripts/RunAll.sh
@@ -1,6 +1,4 @@
 nohup sh MonthlyDiffTime.sh &
-nohup sh MonthlyDiffTime2.sh &
 nohup sh MonthlyRunTime.sh &
-nohup sh MonthlyRunTime2.sh &
-
-nohup sh HeadWayByStop.sh 20120707_20130630.csv 0 9999 &
+nohup sh MonthlyHeadway.sh &
+nohup sh MonthlyAvgWaitTime.sh &
